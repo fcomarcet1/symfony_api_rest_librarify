f8943bfe6bdb908bdedbdbe6aabab55bfacf20c1
fcomarcet1 <fcomarcet1@gmail.com>
2021-07-15 09:35:12 +0200

    Feat: Add upload base64Images with flySystem bundle

--- src/Controller/Api/BookController.php
+++ src/Controller/Api/BookController.php
@@ -3,11 +3,13 @@
 namespace App\Controller\Api;
 
 use App\Entity\Book;
+use App\Form\Model\BookDto;
 use App\Form\Type\BookFormType;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use League\Flysystem\FilesystemOperator;
 use Symfony\Component\HttpFoundation\Request;
 
 class BookController extends AbstractFOSRestController
@@ -24,28 +26,33 @@ class BookController extends AbstractFOSRestController
     }
 
     /**
-     * Create new book.
+     * Create new book opcional upload base64Image.
      *
      * @Rest\Post(path="/books")
      * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
      */
-    public function postAction(Request $request, EntityManagerInterface $em)
-    {
-        //$title = $request->get('title', null);
-        /* $params = json_decode($request->getContent());
-        $title = $params->title;
-        if (empty($title)) {
-            return [
-                'success' => false,
-                'error' => 'Title cannot be empty',
-                'data' => null,
-            ];
-        } */
-        $book = new Book();
-        $form = $this->createForm(BookFormType::class, $book);
+    public function postAction(
+        Request $request,
+        EntityManagerInterface $em,
+        FilesystemOperator $booksStorage
+    ) {
+        // Create new DTO
+        $bookDto = new BookDto();
+        $form = $this->createForm(BookFormType::class, $bookDto);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            //$data = $form->getData();
+
+            // save image(base64Image) in public/storage/images/books
+            $extension = explode('/', mime_content_type($bookDto->base64Image))[1];
+            $data = explode(',', $bookDto->base64Image);
+            $filename = sprintf('%s.%s', uniqid('book_', true), $extension);
+            $booksStorage->write($filename, base64_decode($data[1]));
+
+            $book = new Book();
+            $book->setTitle($bookDto->title);
+            $book->setImage($filename);
             $em->persist($book);
             $em->flush();
 
@@ -53,12 +60,5 @@ class BookController extends AbstractFOSRestController
         }
 
         return $form;
-
-        /* $response = [
-            'success' => true,
-            'data' => $book
-        ];
-        return $response;
-    } */
     }
 }
