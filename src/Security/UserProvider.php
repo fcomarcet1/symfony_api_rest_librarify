61e0950fbd2ec890efef5688b802c68a3bc5a898
fcomarcet1 <fcomarcet1@gmail.com>
2021-07-18 09:54:55 +0200

    Add: user entity && UserProvider

--- src/Security/UserProvider.php
+++ src/Security/UserProvider.php
@@ -55,6 +55,7 @@ class UserProvider implements UserProviderInterface, PasswordUpgraderInterface
             throw new UnsupportedUserException(sprintf('Invalid user class "%s".', get_class($user)));
         }
 
+        return new User();
         // Return a User object after making sure its data is "fresh".
         // Or throw a UsernameNotFoundException if the user no longer exists.
         throw new \Exception('TODO: fill in refreshUser() inside '.__FILE__);
