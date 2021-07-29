4106bd00d7ed17bad11d07e06719508f9057452f
fcomarcet1 <fcomarcet1@gmail.com>
2021-07-29 09:33:46 +0200

    Add: change annotations format to yml in models

--- librarify/src/Entity/Book.php
+++ librarify/src/Entity/Book.php
@@ -2,37 +2,14 @@
 
 namespace App\Entity;
 
-use App\Repository\BookRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity(repositoryClass=BookRepository::class)
- * @ORM\Table(name="books")
- */
 class Book
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
     private $id;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
     private $title;
-
-    /**
-     * @ORM\Column(type="string", length=512, nullable=true)
-     */
     private $image;
-
-    /**
-     * @ORM\ManyToMany(targetEntity=Category::class, inversedBy="books")
-     */
     private $categories;
 
     public function __construct()
--- librarify/src/Entity/Category.php
+++ librarify/src/Entity/Category.php
@@ -2,32 +2,13 @@
 
 namespace App\Entity;
 
-use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity(repositoryClass=CategoryRepository::class)
- * @ORM\Table(name="categories")
- */
 class Category
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
     private $id;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
     private $name;
-
-    /**
-     * @ORM\ManyToMany(targetEntity=Book::class, mappedBy="categories")
-     */
     private $books;
 
     public function __construct()
