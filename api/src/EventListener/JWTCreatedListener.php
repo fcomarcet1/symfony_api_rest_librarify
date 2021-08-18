c2360fe25bcfe58c11c511fb74c7571c54d340a0
fcomarcet1 <fcomarcet1@gmail.com>
2021-08-18 17:20:55 +0200

    Change ttl_token for test

---
+++ api/src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class JWTCreatedListener
+{
+    private $requestStack;
+    private $em;
+
+    public function __construct(RequestStack $requestStack, \Doctrine\ORM\EntityManagerInterface $em)
+    {
+        $this->requestStack = $requestStack;
+        $this->em = $em;
+    }
+
+    public function onJWTCreated(JWTCreatedEvent $event): void
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        $userRepository = $this->em->getRepository(User::class);
+
+        $payload = $event->getData();
+
+        // Si queremos recoger el user del evento necesitammos modificar la interface y crear un getId(): UuidInterface
+        // por eso estoy usando el repository
+        $user = $userRepository->findOneBy(['email' => $payload['username']]);
+
+        $payload['sub'] = $user->getId();
+        $payload['ip'] = $request->getClientIp();
+        $payload['iat'] = time();
+
+        $event->setData($payload);
+    }
+}
---
+++ api/src/EventListener/JWTDecodedListener.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTDecodedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class JWTDecodedListener
+{
+    private RequestStack $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public function onJWTDecoded(JWTDecodedEvent $event)
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        $payload = $event->getPayload();
+
+        // Check if Ip client its same from token ip
+        if (!isset($payload['ip']) || $payload['ip'] !== $request->getClientIp()) {
+            $event->markAsInvalid();
+        }
+
+        // Check if token has expired
+       /*  if (!isset($payload['exp'])) {
+            $event->markAsInvalid();
+        } */
+    }
+}
